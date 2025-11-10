f864922adb1786ba9cc7beeb9494beeb33623b9b
Alexis Belisle <202384667@cegepsherbrooke.qc.ca>
2025-11-10 09:20:34 -0500

    Created game of life in consile with rust

--- code/game_of_life/src/main.rs
+++ code/game_of_life/src/main.rs
@@ -1,3 +1,12 @@
+use std::{thread, time::Duration};
+use game_of_life::Universe;
+
 fn main() {
-    println!("Hello, world!");
+    let mut u = Universe::random(120, 60, 12345);
+    for _ in 0..200 {
+        print!("\x1B[2J\x1B[H"); // clear screen and move cursor home
+        println!("{}", u.as_string());
+        u.tick();
+        thread::sleep(Duration::from_millis(150));
+    }
 }
